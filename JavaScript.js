95999d18b6324e0f89e8b9d278c3946144093db2
Mark Leite <marktobusiness113@gmail.com>
2023-09-08 13:59:45 -0300

    feat: second step logical things

--- JavaScript.js
+++ JavaScript.js
@@ -1,10 +1,40 @@
-const vetor = []
+const vetor = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 
+'Obesidade grau 2', 'Obesidade grau 3']
 
 const calcular = () => {
     let peso = parseFloat(document.querySelector('#pesoBox').value)
     let altura = parseFloat(document.querySelector('#alturaBox').value)
     let res = document.querySelector('.res')
-
+    
     const calculo = (a, b) => a / Math.pow(b, 2)
-    res.innerHTML = `O seu IMC é ${calculo(peso, altura)}`
+    const imc = calculo(peso, altura)
+    let situacao;
+
+    switch (true) {
+        case (imc <= 18.75):
+            situacao = vetor[0]
+            break;
+        case (imc <= 24.9):
+            situacao = vetor[1]
+            break;
+        case (imc <= 29.9):
+            situacao = vetor[2]
+            break;
+        case (imc <= 34.9):
+            situacao = vetor[3]
+            break;
+        case (imc <= 39.9):
+            situacao = vetor[4]
+            break;
+        case (imc >= 40):
+            situacao = vetor[5]
+            break;
+        default:
+            break
+    }
+
+
+    res.innerHTML = `O seu IMC é ${imc.toFixed(2)} (${situacao}).`
+
+    
 }
