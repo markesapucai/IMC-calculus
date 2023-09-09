c51ba8588a652c47d68bb1ee191e2cd78d55a93f
Mark Leite <marktobusiness113@gmail.com>
2023-09-09 11:40:23 -0300

    feat: JavaScript Logical

--- JavaScript.js
+++ JavaScript.js
@@ -1,40 +1,52 @@
-const vetor = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 
-'Obesidade grau 2', 'Obesidade grau 3']
+
 
 const calcular = () => {
-    let peso = parseFloat(document.querySelector('#pesoBox').value)
-    let altura = parseFloat(document.querySelector('#alturaBox').value)
-    let res = document.querySelector('.res')
-    
-    const calculo = (a, b) => a / Math.pow(b, 2)
-    const imc = calculo(peso, altura)
-    let situacao;
+    //validation
+    const pesoBox = document.querySelector('#pesoBox')
+    const alturaBox = document.querySelector('#alturaBox')
 
+    if (!pesoBox.value || !alturaBox.value) {
+        alert('Por favor, insira valores válidos para o peso e altura')
+    }
+    const peso = Number(pesoBox.value)
+    const altura = Number(alturaBox.value)
+
+    const vetor = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 
+    'Obesidade grau 2', 'Obesidade grau 3']
+    //calculo imc
+    const calculo = (a, b) => {
+       return a / Math.pow(b, 2)
+    }    
+    const imc = calculo(peso, altura).toFixed(2);
+    //calculo nivel
+    let nivel;
     switch (true) {
         case (imc <= 18.75):
-            situacao = vetor[0]
+            nivel = vetor[0]
             break;
         case (imc <= 24.9):
-            situacao = vetor[1]
+            nivel = vetor[1]
             break;
         case (imc <= 29.9):
-            situacao = vetor[2]
+            nivel = vetor[2]
             break;
         case (imc <= 34.9):
-            situacao = vetor[3]
+            nivel = vetor[3]
             break;
         case (imc <= 39.9):
-            situacao = vetor[4]
+            nivel = vetor[4]
             break;
         case (imc >= 40):
-            situacao = vetor[5]
+            nivel = vetor[5]
             break;
         default:
             break
     }
-
-
-    res.innerHTML = `O seu IMC é ${imc.toFixed(2)} (${situacao}).`
-
-    
-}
+    //coletar e montar resposta
+    const setResultado = (x, y) => {
+        const res = document.querySelector('.res')
+        res.innerHTML = '';
+        res.innerHTML += `O seu IMC é ${x} (${y}).`
+    }
+    setResultado(imc, nivel)
+}   
